9eacdcbea64e7a1a738e18329dcf5121c22283d4
Arfan Rehab <areha308@gmail.com>
2016-12-16 22:37:31 -0800

    More linkedlist

--- LinkedList/LList.java
+++ LinkedList/LList.java
@@ -1,4 +1,75 @@
+import java.util.*;
+
 public class LList {
+
+	// delete duplicates from a linkedlist
+	public static LinkedListNode deleteDups(LinkedListNode n) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		LinkedListNode previous = null;
+		while (n != null) {
+			if (set.contains(n.data)) {
+				previous.next = n.next;
+			}
+			else {
+				set.add(n.data);
+				previous = n;
+			}
+			n = n.next;
+		}
+
+		return previous;
+	}
+
+	// recursive solution for kth to last
+	class Index {
+		public int value = 0;
+	}
+
+	public static LinkedListNode kthToLast(LinkedListNode head, int k) {
+		Index idx = new Index();
+		return kthToLast(head, k, idx);
+	}
+
+	public static LinkedListNode kthToLast(LinkedListNode head, int k, Index idx) {
+		if (head == null) {
+			return null;
+		}
+		LinkedListNode node = kthToLast(head.next, k, idx);
+		idx.value += 1;
+		if (idx.value == k) {
+			return head;
+		}
+		return node;
+	}
+
+	// iterative solution for kth to last
+	public static LinkedListNode nthToLast(LinkedListNode head, int k) {
+		LinkedListNode p1 = head;
+		LinkedListNode p2 = head;
+
+		for (int i = 0; i < k; i++) {
+			if (p1 == null) return null;
+			p1 = p1.next;
+		}
+
+		while (p1 != null) {
+			p1 = p1.next;
+			p2 = p2.next;
+		}
+
+		return p2;
+	}
+
+	// delete middle node 
+	public static boolean deleteNode(LinkedListNode n) {
+		if (n == null || n.next == null) {
+			return false;
+		}
+		LinkedListNode next = n.next;
+		n.data = next.data;
+		n.next = next.next;
+		return true;
+	}
 
 	public static void main(String[] args) {
 
