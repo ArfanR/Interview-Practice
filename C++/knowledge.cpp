0d1241ce76332091e44fa04162fa35c8b4cb7307
Arfan Rehab <areha308@gmail.com>
2017-01-29 03:25:06 -0800

    Shallow vs deep copy

--- C++/knowledge.cpp
+++ C++/knowledge.cpp
@@ -54,6 +54,13 @@ address of the function when the virtual function is called. Dynamic binding in 
 mechanism. Thus, when we assign the derived class object to the base class pointer, the vptr variable points to the
 vtable of the derived class. This assignment ensures that the most derived virtual function gets called.
 
+Shallow vs Deep copy
+A shallow copy copies all the member values from one object to another. A deep copy does all this and also deep copies any
+pointer objects. Note that sha11ow_copy may cause a lot of programming runtime errors, especially with the creation and
+deletion of objects. Shallow copy should be used very carefully and only when a programmer really understands what he wants
+to do. In most cases, shallow copy is used when there is a need to pass information about a complex structure without
+actual duplication of data. One must also be careful with destruction of objects in a shallow copy. In real life, shallow
+copy is rarely used. Deep copy should be used in most cases, especially when the size of the copied  structure is small.
 */
 
 
