b1c34c223e6679987aa0a34b53f76b02e8121bd7
Arfan Rehab <areha308@gmail.com>
2017-06-06 03:28:26 -0700

    balanced tree

--- Practice/Trees.java
+++ Practice/Trees.java
@@ -86,5 +86,23 @@ public class Trees {
         return result;
     }
 
+    public static int GetHeight(TreeNode root) {
+        if (root == null) return -1;
+        return Math.max(getHeight(root.left), getHeight(root.right)) + 1;
+    }
+
+    // check is binary tree is balanced
+    public static boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        int heightDiff = getHeight(root.left) - getHeight(root.right);
+        if (Math.abs(heightDiff) > 1) {
+            return false;
+        } else {
+            return isBalanced(root.left) && isBalanced(root.right);
+        }
+    }
 
 }
+
+
+
