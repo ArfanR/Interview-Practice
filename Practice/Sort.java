8d24b8837b824c4b1eca96a57faead306f7aeb32
Arfan Rehab <areha308@gmail.com>
2017-06-12 09:24:07 -0700

    Anagrams

--- Practice/Sort.java
+++ Practice/Sort.java
@@ -18,4 +18,30 @@ class Sort {
             indexMerged--;
         }
     }
+
+    String sortChars(String s) {
+        char[] content = s.toCharArray();
+        Arrays.sort(content);
+        return new String(content);
+    }
+
+    // sort array so that anagrams are next to each other
+    public static void sort(String[] array) {
+        HashMapList<String, String> mapList = new HashMapList<String, String>();
+
+        for (String s : array) {
+            String key = sortChars(s);
+            mapList.put(key, s);
+        }
+
+        int index = 0;
+        for (String key : mapList.keySet()) {
+            ArrayList<String> list = mapList.get(key);
+            for (String t : list) {
+                array[index] = t;
+                index++;
+            }
+        }
+    }
+
 }
